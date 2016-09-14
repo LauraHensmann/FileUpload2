27f868a386b0b15a44131157ac62821e4d4d9798
LauraHensmann <l.hensmann@gmail.com>
2016-09-14 16:23:45 +0200

    refactoring: add functions

--- upload.php
+++ upload.php
@@ -6,16 +6,18 @@ if (!$_FILES['file']['name']){
 	exit();
 }
 
-$allowedExts = array("png", "jpg", "jpeg", "gif");
-$extension = strtolower(end(explode(".", $_FILES["file"]["name"]))); // nutzt den Dateinamen um die Dateiendung zu bestimmen. Um Fehler zu vermeiden, wird die Dateiendung mit strtolower zu Kleinbuchstaben umgewandelt.
-
-// set image name
-if(isset($_POST['imagename']) && !empty($_POST['imagename'])) { // Prüft ob Variable gesetzt und nicht leer ist. (! negiert die Anweisung)
-	$imagename = mysql_real_escape_string($_POST['imagename']);
-} else {
-	$imagename = mysql_real_escape_string($_FILES['file']['name']);
+function checkFile(){
+	if (!exif_imagetype($_FILES['file']['tmp_name'])){ // liest die ersten bytes des Bildes aus und überprüft die Signatur -> gibt "false" aus wenn nicht
+		$error = 1;
+		
+		header("Location: index.php?error=".$error);
+		exit();
+	}
 }
-	
+
+function checkFileExtension() {
+	$allowedExts = array("png", "jpg", "jpeg", "gif");
+	$extension = strtolower(end(explode(".", $_FILES["file"]["name"]))); // nutzt den Dateinamen um die Dateiendung zu bestimmen. Um Fehler zu vermeiden, wird die Dateiendung mit strtolower zu Kleinbuchstaben umgewandelt.
 	// $imagename = str_replace("/","_","$imagename");  // (FUNKTIONIERT wurde durch preg_match ersetzt)
 			
 	$pattern = '/([^\w\d._-])/i';
@@ -29,36 +31,42 @@ if(isset($_POST['imagename']) && !empty($_POST['imagename'])) { // Prüft ob Var
 			echo "Return Code: " . $_FILES["file"]["error"] . "<br />";	// andernfalls wird eine Fehlermeldung ausgegeben.
 		}
 	}
-	
-	if (!exif_imagetype($_FILES['file']['tmp_name'])){ // liest die ersten bytes des Bildes aus und überprüft die Signatur -> gibt "false" aus wenn nicht
-		$error = 1;
-		
-		header("Location: index.php?error=".$error);
-		exit();
-	}
-	
-	echo "Upload: " . $_FILES["file"]["name"] . "<br />";
-	echo "Type: " . $_FILES["file"]["type"] . "<br />";
-	echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
-	echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br />";
-		
-	$deployOK = 0; $deployTries = 1;
-	while($deployOK == 0){
+	return $extension;
+}
 
-	if(file_exists("uploads/" . $imagename.".".$extension)) {
-			if(strpos($imagename, '_'.$deployTries) !== false) {  //Falls der Ordner bereits bereits "x" enthält, entferne und versuche erneut
-				$imagename = rtrim($imagename,"_".$deployTries);
-				$deployTries++;									// erhöht Anzahl der Speicherversuche
-				$imagename = $imagename."_".$deployTries;		// setze _$Versuche hinter Bildname
-			}else{						
-				$imagename = $imagename."_".$deployTries;		// Falls erster Versuch setze _1 hinter Bildname
-			}
-			continue;
-		} 
-			
-		$deployOK = 1;										//Falls noch nicht vorhanden, speichern
-		move_uploaded_file($_FILES["file"]["tmp_name"],"uploads/{$imagename}.{$extension}");
-		echo "Stored in: uploads/". $_FILES["file"]["name"];
-		header("Location: index.php");
-	}
+// set image name
+if(isset($_POST['imagename']) && !empty($_POST['imagename'])) { // Prüft ob Variable gesetzt und nicht leer ist. (! negiert die Anweisung)
+	$imagename = escapeshellcmd($_POST['imagename']);
+} else {
+	$imagename = escapeshellcmd($_FILES['file']['name']);
+}
+	
+$extension = checkFileExtension(); // eingentlich nicht nötig
+checkFile();
+
+echo "Upload: " . $_FILES["file"]["name"] . "<br />";
+echo "Type: " . $_FILES["file"]["type"] . "<br />";
+echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
+echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br />";
+	
+$deployOK = 0; $deployTries = 1;
+while($deployOK == 0){
+
+if(file_exists("uploads/" . $imagename.".".$extension)) {
+		if(strpos($imagename, '_'.$deployTries) !== false) {  //Falls der Ordner bereits bereits "x" enthält, entferne und versuche erneut
+			$imagename = rtrim($imagename,"_".$deployTries);
+			$deployTries++;									// erhöht Anzahl der Speicherversuche
+			$imagename = $imagename."_".$deployTries;		// setze _$Versuche hinter Bildname
+		}else{						
+			$imagename = $imagename."_".$deployTries;		// Falls erster Versuch setze _1 hinter Bildname
+		}
+		continue;
+	} 
+		
+	$deployOK = 1;										//Falls noch nicht vorhanden, speichern
+	move_uploaded_file($_FILES["file"]["tmp_name"],"uploads/{$imagename}.{$extension}");
+	echo "Stored in: uploads/". $_FILES["file"]["name"];
+	header("Location: index.php");
+}	
+
 ?>
